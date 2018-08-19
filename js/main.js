c2646beb74d54f3987cb37b16dd3aeaccb96156a
Hari Narayanan <smgdark@gmail.com>
2018-08-19 11:29:23 +0530

    added customerly

--- js/main.js
+++ js/main.js
@@ -9,6 +9,31 @@ load("./js/compiler.js");
 load("./js/script.js");
 
 
+// Plugins
+
+// Customerly Integration Code
+window.customerlySettings = {
+    app_id: "0fe5a52c"
+};
+! function() {
+    function e() {
+        var e = t.createElement("script");
+        e.type = "text/javascript", e.async = !0,
+            e.src = "https://widget.customerly.io/widget/0fe5a52c";
+        var r = t.getElementsByTagName("script")[0];
+        r.parentNode.insertBefore(e, r)
+    }
+    var r = window,
+        t = document,
+        n = function() {
+            n.c(arguments)
+        };
+    r.customerly_queue = [], n.c = function(e) {
+            r.customerly_queue.push(e)
+        },
+        r.customerly = n, r.attachEvent ? r.attachEvent("onload", e) : r.addEventListener("load", e, !1)
+}();
+
 // Cookie Consent
 window.addEventListener("load", function() {
     window.cookieconsent.initialise({
