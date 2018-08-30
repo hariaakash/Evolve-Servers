5661d6ada2848b0a4e8ae49308679db72caa8cff
Hari Narayanan <smgdark@gmail.com>
2018-08-31 00:14:22 +0530

    moving js to main.js 1

--- new/js/main.js
+++ new/js/main.js
@@ -10,3 +10,5 @@ load("../plugins/slider/js/smartslider-frontend.min.js");
 load("../plugins/slider/js/smartslider-simple-type-frontend.min.js");
 load("../plugins/slider/js/nextend-webfontloader.min.js");
 load("./js/slider.js");
+load("https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js");
+load("./js/slider.js");
