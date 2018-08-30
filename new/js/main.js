f684efb53e00be63a8d91f796660f31241ebaeb1
Hari Narayanan <smgdark@gmail.com>
2018-08-31 00:01:49 +0530

    moved necessary css to main.css

---
+++ new/js/main.js
@@ -0,0 +1,12 @@
+function load(script) {
+    document.write('<' + 'script src="' + script + '" type="text/javascript"><' + '/script>');
+}
+
+
+load("../plugins/slider/js/n2-j.min.js");
+load("../plugins/slider/js/nextend-gsap.min.js");
+load("../plugins/slider/js/nextend-frontend.min.js");
+load("../plugins/slider/js/smartslider-frontend.min.js");
+load("../plugins/slider/js/smartslider-simple-type-frontend.min.js");
+load("../plugins/slider/js/nextend-webfontloader.min.js");
+load("./js/slider.js");
